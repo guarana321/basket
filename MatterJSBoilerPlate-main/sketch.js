59da4e6f4657e680d6d2471a5854a59623a90e0d
guarana321 <99995923+guarana321@users.noreply.github.com>
2022-11-05 18:53:39 -0300

    Add files via upload

---
+++ MatterJSBoilerPlate-main/sketch.js
@@ -0,0 +1,44 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ball;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+var ball_options={
+	isStatic:false,
+	restitution:0.3,
+	friction:0,
+	desity:1.2
+}
+
+	engine = Engine.create();
+	world = engine.world;
+	groundObj.display();
+	groundObj=new ground(width/2,670,width,20);
+	leftSide = new ground(1100,600,20,120);
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
